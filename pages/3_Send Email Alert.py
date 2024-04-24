50b73b90111329dc18d2795e6ac662735192d4c4
Divya Rani <101196300+divy08r@users.noreply.github.com>
2024-04-24 20:46:09 +0530

    Update 3_Send Email Alert.py

--- pages/3_Send Email Alert.py
+++ pages/3_Send Email Alert.py
@@ -6,7 +6,7 @@ import firebase_admin
 from firebase_admin import credentials, db, firestore
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("credentials.json")
+    cred = credentials.Certificate("mycredentials.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
